56aa5875b378bc8b5543a05ab457f7032a2ee9ab
Tobi John <connect@tobijohn.com>
2023-02-03 10:29:56 +0100

    added functionality to the arithmetic operations

--- iphone.js
+++ iphone.js
@@ -4,14 +4,13 @@ const clear = document.getElementById("clear");
 const operation = document.querySelectorAll("#operation");
 const equals = document.getElementById("equal");
 let input = [];
-// let allNumbers; I was trying to put all numbers in an array here, but e no wan cooperate
-//i decided to understand how single works first before proceeding to calculate more that 2 numbers.
-let firstNumber = [];
-let secondNumber = []; 
 let allNumber = [];
-let answer;
+let convNumber = [];
 let isExist = false; 
+let isResult = false;
 let result;
+let answer;
+let operationSign = [];
 
 renderApp();
 
@@ -21,17 +20,24 @@ function renderApp()
 {
     input = [];
     for(let i = 0; i < numbers.length; i++){
-        numbers[i].addEventListener('click', function runSimulation(){
+        numbers[i].addEventListener('click', function runSimulation(e){
 
             if(resultDisplay.textContent === "0" || isExist === false){
                 input.push(numbers[i].textContent);
-                result = parseInt(input.join(""));
+                result = input.join("");
                 resultDisplay.textContent = result;
             }
-           
+
+            else if(isResult === true){
+                allNumber.push(answer);
+                input.push(numbers[i].textContent);
+                result = input.join("");
+                resultDisplay.textContent = result; 
+            }
+            
             else if(isExist === true){ 
                 input.push(numbers[i].textContent);
-                result = parseInt(input.join(""));
+                result = input.join("");
                 resultDisplay.textContent = result;
             }
         });
@@ -42,11 +48,21 @@ function renderApp()
 for(let i = 0; i < operation.length; i++){
     let arithmeticSign = operation[i];
     arithmeticSign.addEventListener("click", ()=>{
-        allNumber.push(result);
-        console.log("Stored number: " + allNumber);
-        console.log("THis is the operation: " + arithmeticSign.textContent)
-        isExist = true;
-        input = []//you were the problem na wa o
+        
+        if(isResult === true){
+            renderApp.runSimulation();
+        }
+        else{
+            allNumber.push(result);
+            // arithmeticSign = arithmeticSign.textContent;
+            console.log("Stored number: " + allNumber);
+            console.log("THis is the operation: " + arithmeticSign.textContent);
+            operationSign = arithmeticSign.textContent;
+            isExist = true;
+            input = []
+        }
+
+        
      });
 }
 
@@ -54,13 +70,61 @@ equals.addEventListener("click", solveEquation);
 
 function solveEquation(){
     allNumber.push(result);
-    console.log(allNumber);
-    for(let i =0; i < allNumber.length; i++){
-        answer = answer + allNumber[i];
+    convNumber = allNumber.map(Number);
+    console.log(convNumber);
+    
+    
+    if(operationSign[0] === "+"){
+        answer = convNumber.reduce(function(initialNumber, numberToAdd){
+            return initialNumber + numberToAdd;
+        })
+        resultDisplay.textContent = answer;
+        console.log(answer);
+        allNumber = [];
+        operationSign = [];
+        isResult = true;
+        
+
+    }
+    else if(operationSign[0] === "-"){
+        answer = convNumber.reduce(function(initialNumber, numberToAdd){
+            return initialNumber - numberToAdd;
+        })
+        resultDisplay.textContent = answer;
+        console.log(answer);
+        allNumber = [];
+        operationSign = [];
+        isResult = true;
         
     }
+
+    else if(operationSign[0] === "x"){
+        answer = convNumber.reduce(function(initialNumber, numberToAdd){
+            return initialNumber * numberToAdd;
+        })
+        resultDisplay.textContent = answer;
+        console.log(answer);
+        allNumber = [];
+        operationSign = [];
+        isResult = true;
+        
+    }
+
+    else if(operationSign[0] === "/"){
+        answer = convNumber.reduce(function(initialNumber, numberToAdd){
+            return initialNumber / numberToAdd;
+        })
+        resultDisplay.textContent = answer;
+        console.log(answer);
+        allNumber = [];
+        operationSign = [];
+        isResult = true;
+        
+    }
+    else{
+        console.log("Operation not found");
+    }
     
-    console.log(answer);
 }
 
 
